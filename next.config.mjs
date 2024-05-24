59e925c31dae7d96c8fa97f0614e4b8a49438420
Elpidio Mazza <49283847+Elpiu@users.noreply.github.com>
2024-05-24 19:28:45 +0200

    up

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+
 const nextConfig = {
     output: "export",
-    distDir: 'docs',
     images: {
         domains: ['flowbite.s3.amazonaws.com'],
     }
